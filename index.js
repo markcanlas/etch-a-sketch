ba7e67f289983653d6ee292b3f3f9cd8887e5ef0
markcanlas <m4rkd0tc0de@gmail.com>
2021-09-17 15:10:23 +0800

    Add Buttons Function

--- index.js
+++ index.js
@@ -1,41 +1,81 @@
-const body = document.querySelector('body')
-const container = document.querySelector('.container');
-let btn = document.createElement('button');
+DEFAULTSIZE = 16;
+DEFAULTCOLOR = '#f1f3f5';
 
 
-body.prepend(btn)
+let size = DEFAULTSIZE;
+let color = DEFAULTCOLOR;
 
-//creating 16x16 grid of squares
-const square = (cols, rows) => {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
+const body = document.querySelector("body");
+const box = document.querySelector(".boxes");
+const boxes = document.querySelector(".squares");
+let btn = document.createElement("button");
+const clearbtn = document.querySelector(".btn-clear");
+const colorbtn = document.querySelector('input');
+const sliderbtn = document.getElementById('sizeSlider')
+const colorValue = document.getElementById('myColor')
 
-    for(i = 0 ; i < (rows * cols); i++){
-        let div = document.createElement('div')
-        container.appendChild(div).className = 'squares';
-    }
-}
 
-square(16,16);
- 
+
+
+//change size of grids
+// sliderbtn.addEventListener('change', e => {
+//     let size = e.target.value
+//     clearGrid();
+// })
+square(16, 16);
 
 //adding event listener to hover and change background color to the squares
-container.addEventListener('mouseover', event => {
-    let hover = event.target.className;
 
-    if (hover === 'squares') {
-        event.target.style.backgroundColor = 'black';
-    }
+
+
+//clearing the grid
+clearbtn.addEventListener("click", e => {
+    clearGrid();
+});
+
+colorbtn.addEventListener('change', e => {
+    let pickColor = colorValue.value;
+    switchColor(pickColor);
 })
 
+//creating 16x16 grid of squares
+function square(size, size) {
+    box.style.setProperty("--grid-rows", size);
+    box.style.setProperty("--grid-cols", size);
+    
+    for (i = 0; i < size * size; i++) {
+        let div = document.createElement("div");
+        box.appendChild(div).className = "squares";
+    }
+    
+};
 
 
+box.addEventListener("mouseover", (event) => {
+    let hover = event.target.className;
+        
+    if (hover === "squares") {
+         event.target.style.backgroundColor = 'black';
+        } 
+    });
 
 
+function switchColor(colorPicker) {
+    box.addEventListener("mouseover", (event) => {
+        let hover = event.target.className;
+            
+        if (hover === "squares") {
+             event.target.style.backgroundColor = colorPicker;
+            } 
+        });
+}
 
-
-
-
+function clearGrid() {
+    let grids = box.querySelectorAll('.squares');
+    grids.forEach(grid => grid.style.cssText = '1px solid #f1f3f5');
+    switchColor('black');
+    colorValue.value;
+}
 
 
 
