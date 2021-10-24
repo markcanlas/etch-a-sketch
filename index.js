d086ee397643691200f5af8bc5073d77bfe0b728
mark <m4rkd0tc0de@gmail.com>
2021-10-25 04:30:00 +0800

    partially done

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-DEFAULTSIZE = 16;
-DEFAULTCOLOR = '#333333';
-DEFAULTMODE = 'color'
-DEFAULTGRID = 'grid'
-
+DEFAULTSIZE = 32;
+DEFAULTCOLOR = "#333333";
+DEFAULTMODE = "color";
+DEFAULTBOARD = "classic";
 
 let currentSize = DEFAULTSIZE;
 let currentColor = DEFAULTCOLOR;
 let currentMode = DEFAULTMODE;
-let currentGrid = DEFAULTGRID; 
+let currentBoard = DEFAULTBOARD;
 
 function setCurrentColor(newColor) {
   currentColor = newColor;
@@ -21,96 +20,120 @@ function setCurrentMode(newMode) {
   currentMode = newMode;
 }
 
-function setCurrentGrid(newGrid) {
-  currentGrid = newGrid;
+function setCurrentBoard(newBoard) {
+  currentBoard = newBoard;
 }
 
-
-const body = document.querySelector("body");
+const buttons = document.querySelectorAll("button");
 const box = document.querySelector(".boxes");
+const boxChilds = document.querySelectorAll(".all-box");
 const clearBtn = document.querySelector(".btn-clear");
-const sliderBtn = document.getElementById('sizeSlider');
-const colorValue = document.getElementById('myColor');
-const colorBtn = document.querySelector('.colorBtn')
-const label = document.querySelector('label');
-const rainbowBtn = document.querySelector('.rainbowBtn');
-const eraserBtn = document.querySelector('.eraserBtn');
-const classicBtn = document.querySelector('.classic');
-const gridStyleBtn = document.querySelector('.grid-style');
+const sliderBtn = document.getElementById("sizeSlider");
+const colorValue = document.getElementById("myColor");
+const colorText = document.querySelector(".colorText");
+const label = document.querySelector(".label");
+const rainbowBtn = document.querySelector(".rainbowBtn");
+const eraserBtn = document.querySelector(".eraserBtn");
+const classicBtn = document.querySelector(".classic");
+const gridStyleBtn = document.querySelector(".grid-style");
 
-
+colorValue.onchange = (e) => setCurrentColor(e.target.value);
+sliderBtn.onchange = (e) => changeSize(e.target.value);
+sliderBtn.onmousemove = (e) => updateSizeValue(e.target.value);
 clearBtn.onclick = () => reloadGrid();
-colorValue.onchange = e => setCurrentColor(e.target.value);
-sliderBtn.onchange = e => changeSize(e.target.value);
-sliderBtn.onmousemove = e => updateSizeValue(e.target.value);
-rainbowBtn.onclick = () => setCurrentMode('rainbow');
-classicBtn.onclick = () => square(currentSize, 'classic');
-gridStyleBtn.onclick = () => square(currentSize, 'grid')
-
-
-
-// function changePlatform(platform) {
-//   if( platform === 'classic') {
-//     reloadGrid()
-//     for(let i = 0; i <= box.children.length; i++) {
-//       box.children[i].classList.remove('squares')
-//    }
-// } else if (platform === "grid") {
-//   reloadGrid()
-// }
-// }
-
+rainbowBtn.addEventListener("click", clickHandler);
+colorValue.addEventListener("click", clickHandler);
+eraserBtn.addEventListener("click", clickHandler);
+classicBtn.addEventListener("click", clickHandler);
+gridStyleBtn.addEventListener("click", clickHandler);
 
 function clearGrid() {
-    box.innerHTML = ``;
+  box.innerHTML = ``;
 }
 
 function reloadGrid() {
-    clearGrid();
-    square(currentSize, currentGrid);
+  clearGrid();
+  square(currentSize, currentBoard);
 }
 
-function changeSize(value){
-    setCurrentSize(value)
-    updateSizeValue(value)
-    reloadGrid()
+function changeSize(value) {
+  setCurrentSize(value);
+  updateSizeValue(value);
+  reloadGrid();
 }
-
 
 function updateSizeValue(value) {
-    label.innerHTML =`${value} x ${value}`;
+  label.innerHTML = `${value} x ${value}`;
 }
 
+function clickHandler(event) {
+  const el = event.target;
+  const removeMode = buttons.forEach((button) =>
+    button.classList.remove("_mode")
+  );
+  const removeTextMode = colorText.classList.remove("_mode");
 
-function square(size, platform) {
-    box.style.setProperty("--grid-rows", size);
-    box.style.setProperty("--grid-cols", size);
-   
-    for (i = 0; i < size * size; i++) {
-      const div = document.createElement("div");
-      div.classList.add('squares')
-      div.addEventListener('mouseover', switchColor)
-      box.appendChild(div)
-    }
-
-
-
- console.log(platform)
-};
-
-function switchColor(colour) {
-  if (currentMode === 'rainbow') {
-    const randomR = Math.floor(Math.random() * 256)
-    const randomG = Math.floor(Math.random() * 256)
-    const randomB = Math.floor(Math.random() * 256)
-    colour.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
-  } else if (currentMode === 'color') {
-    colour.target.style.backgroundColor = currentColor
-  } else if (currentMode === 'eraser') {
-    colour.target.style.backgroundColor = '#fefefe'
+  if (el.contains(rainbowBtn)) {
+    setCurrentMode("rainbow");
+    removeMode;
+    removeTextMode;
+    rainbowBtn.classList.add("_mode");
+  } else if (el.contains(colorValue)) {
+    setCurrentMode("color");
+    removeMode;
+    colorText.classList.add("_mode");
+  } else if (el.contains(eraserBtn)) {
+    setCurrentMode("eraser");
+    removeMode;
+    removeTextMode;
+    eraserBtn.classList.add("_mode");
+  } else if (el.contains(classicBtn)) {
+    clearGrid();
+    setCurrentBoard("classic");
+    square(currentSize, "classic");
+    removeMode;
+    removeTextMode;
+    classicBtn.classList.add("_mode");
+  } else if (el.contains(gridStyleBtn)) {
+    setCurrentBoard("grid");
+    clearGrid();
+    square(currentSize, "grid");
+    removeMode;
+    removeTextMode;
+    gridStyleBtn.classList.add("_mode");
   }
 }
-  
-window.onload = () => {
-    square(DEFAULTSIZE, DEFAULTGRID)
+
+function square(size, name) {
+  box.style.setProperty("--grid-rows", size);
+  box.style.setProperty("--grid-cols", size);
+  for (i = 0; i < size * size; i++) {
+    const div = document.createElement("div");
+    div.addEventListener("mouseover", switchColor);
+    box.appendChild(div);
+    if (name === "grid") {
+      div.classList.remove("remove-squares");
+      div.classList.add(`squares`);
+    } else if (name === "classic") {
+      div.classList.remove("squares");
+      div.classList.add("remove-squares");
+    }
+  }
 }
+
+function switchColor(colour) {
+  if (currentMode === "rainbow") {
+    const randomR = Math.floor(Math.random() * 256);
+    const randomG = Math.floor(Math.random() * 256);
+    const randomB = Math.floor(Math.random() * 256);
+    colour.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+  } else if (currentMode === "color") {
+    colour.target.style.backgroundColor = currentColor;
+  } else if (currentMode === "eraser") {
+    colour.target.style.backgroundColor = "red";
+  }
+}
+
+window.onload = () => {
+  square(DEFAULTSIZE, DEFAULTBOARD);
+};
